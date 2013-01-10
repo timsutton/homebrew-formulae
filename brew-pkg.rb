eb55978151f2e415873c18f22707d72205e180d9
Timothy Sutton <tim@synthist.net>
2013-01-10 14:15:13 -0500

    brew-pkg v0.1.0

---
+++ brew-pkg.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class BrewPkg < Formula
+  homepage 'https://github.com/timsutton/brew-pkg'
+  url 'https://github.com/timsutton/brew-pkg.git', :tag => 'v0.1.0'
+  version '0.1.0'
+
+  # This is an .rb that must be executable in order for Homebrew to
+  # find it with the 'which' method, so we skip_clean
+  skip_clean 'bin'
+
+  def install
+    bin.install 'brew-pkg.rb'
+  end
+end
