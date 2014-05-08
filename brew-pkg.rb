067fedc0a4f72c3f13a103808234232fcf5c52dd
Timothy Sutton <tim@synthist.net>
2014-05-08 16:31:12 -0400

    Update brew-pkg.rb
    brew-pkg -> 0.1.4

--- brew-pkg.rb
+++ brew-pkg.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class BrewPkg < Formula
   homepage 'https://github.com/timsutton/brew-pkg'
-  url 'https://github.com/timsutton/brew-pkg.git', :tag => 'v0.1.3'
-  version '0.1.3'
+  url 'https://github.com/timsutton/brew-pkg.git', :tag => 'v0.1.4'
+  version '0.1.4'
 
   # This is an .rb that must be executable in order for Homebrew to
   # find it with the 'which' method, so we skip_clean
