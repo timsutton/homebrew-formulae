e3fb8cc5a4083edebae3109afb0b673fbc874909
Timothy Sutton <tim@synthist.net>
2016-01-31 07:55:47 -0500

    Style fixes for brew-pkg.rb

--- brew-pkg.rb
+++ brew-pkg.rb
@@ -1,15 +1,14 @@
-require 'formula'
-
 class BrewPkg < Formula
-  homepage 'https://github.com/timsutton/brew-pkg'
-  url 'https://github.com/timsutton/brew-pkg.git', :tag => 'v0.1.5'
-  version '0.1.5'
+  desc "Homebrew command for building OS X packages from installed formulae."
+  homepage "https://github.com/timsutton/brew-pkg"
+  url "https://github.com/timsutton/brew-pkg.git", :tag => "v0.1.5",
+                                                   :revision => "5b630de615c46c3c419de34fccfeedafcb0c7fd5"
 
   # This is an .rb that must be executable in order for Homebrew to
   # find it with the 'which' method, so we skip_clean
-  skip_clean 'bin'
+  skip_clean "bin"
 
   def install
-    bin.install 'brew-pkg.rb'
+    bin.install "brew-pkg.rb"
   end
 end
