9c0d24837470dc118875529346ed1be8611cec1b
Timothy Sutton <tim@synthist.net>
2017-06-12 16:48:43 -0400

    brew-pkg 0.1.6

--- brew-pkg.rb
+++ brew-pkg.rb
@@ -1,8 +1,8 @@
 class BrewPkg < Formula
   desc "Homebrew command for building OS X packages from installed formulae."
   homepage "https://github.com/timsutton/brew-pkg"
-  url "https://github.com/timsutton/brew-pkg.git", :tag => "v0.1.5",
-                                                   :revision => "5b630de615c46c3c419de34fccfeedafcb0c7fd5"
+  url "https://github.com/timsutton/brew-pkg.git", :tag => "v0.1.6",
+                                                   :revision => "ac9e3885c01dca0616fe3cec1530fc153da3271b"
 
   # This is an .rb that must be executable in order for Homebrew to
   # find it with the 'which' method, so we skip_clean
