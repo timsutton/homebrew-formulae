10ed93d3fe2b4d6275e934d01157c4fe3b1ca67b
Timothy Sutton <tim@synthist.net>
2021-10-29 13:35:59 -0400

    remove bottle :unneeded from pngout

--- pngout.rb
+++ pngout.rb
@@ -4,8 +4,6 @@ class Pngout < Formula
   url "https://www.jonof.id.au/files/kenutils/pngout-20200115-macos.zip"
   sha256 "3272fa947eeafc1ac0086e1f14935f9efa21d090a555e6892255fe5c88f6686e"
 
-  bottle :unneeded
-
   def install
     bin.install "pngout"
   end
