ff8e2c346340b2ba6ea1718f6ddfe778fadced4a
Timothy Sutton <tim@synthist.net>
2022-11-03 20:03:43 -0400

    speedwagon 0.2.0

--- speedwagon.rb
+++ speedwagon.rb
@@ -2,8 +2,8 @@ class Speedwagon < Formula
   desc "Download iOS/tvOS/watchOS simulators without Xcode"
   homepage "https://github.com/timsutton/speedwagon"
   url "https://github.com/timsutton/speedwagon.git",
-      tag:      "v0.1.0",
-      revision: "60cd66c367a26918d894c37897899e1b0f7b8e7f"
+      tag:      "v0.2.0",
+      revision: "d32c4edfb847a2a01553b537d782f1b94db28809"
   license "MIT"
   head "https://github.com/timsutton/speedwagon.git", branch: "main"
 
