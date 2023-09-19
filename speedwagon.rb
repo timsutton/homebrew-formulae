862106e22f44509cddbe30f61fc06b54075e69ad
Timothy Sutton <tim@synthist.net>
2023-09-19 06:26:11 -0400

    speedwagon: support both prebuilt binaries and head

--- speedwagon.rb
+++ speedwagon.rb
@@ -5,12 +5,22 @@ class Speedwagon < Formula
       tag:      "v0.2.0",
       revision: "d32c4edfb847a2a01553b537d782f1b94db28809"
   license "MIT"
+
   head "https://github.com/timsutton/speedwagon.git", branch: "main"
+
+  stable do
+    url "https://github.com/timsutton/speedwagon/releases/download/v0.2.0/speedwagon_0.2.0_darwin_all.tar.gz"
+    sha256 "eeea97d399edc1b3983adab16a7ead1c3dd3fdb99018699f92de4993337b5834"
+  end
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w")
+    if build.head?
+      system "go", "build", *std_go_args(ldflags: "-s -w")
+    else
+      bin.install "speedwagon"
+    end
   end
 
   test do
