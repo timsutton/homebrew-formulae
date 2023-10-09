e0a2a898637ba14cae470b523975164d178ba6d9
Timothy Sutton <tim@synthist.net>
2023-10-09 08:49:43 -0400

    Fix brew audit on brew-pkg

--- brew-pkg.rb
+++ brew-pkg.rb
@@ -1,8 +1,8 @@
 class BrewPkg < Formula
   desc "Homebrew command for building OS X packages from installed formulae"
   homepage "https://github.com/timsutton/brew-pkg"
-  url "https://github.com/timsutton/brew-pkg.git", :tag => "v0.1.7",
-                                                   :revision => "157dbe8b4696db890517ec3b6bf0c9aa02aedd82"
+  url "https://github.com/timsutton/brew-pkg.git", tag:      "v0.1.7",
+                                                   revision: "157dbe8b4696db890517ec3b6bf0c9aa02aedd82"
 
   # This is an .rb that must be executable in order for Homebrew to
   # find it with the 'which' method, so we skip_clean
