5f40ebffb02a6e3e312349af880c020f8ce7f406
Timothy Sutton <tim@synthist.net>
2023-10-09 08:48:38 -0400

    Fix brew audit on speedwagon

--- speedwagon.rb
+++ speedwagon.rb
@@ -1,9 +1,6 @@
 class Speedwagon < Formula
   desc "Download iOS/tvOS/watchOS simulators without Xcode"
   homepage "https://github.com/timsutton/speedwagon"
-  url "https://github.com/timsutton/speedwagon.git",
-      tag:      "v0.2.0",
-      revision: "d32c4edfb847a2a01553b537d782f1b94db28809"
   license "MIT"
 
   head "https://github.com/timsutton/speedwagon.git", branch: "main"
