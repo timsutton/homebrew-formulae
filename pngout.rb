319fe90af34354d560cef364bb3bb6ced18c52af
Timothy Sutton <tim@synthist.net>
2023-10-09 08:44:42 -0400

    Update to March 22 2023 (Universal2) mac zip of pngout
    - also add functional test

--- pngout.rb
+++ pngout.rb
@@ -1,10 +1,14 @@
 class Pngout < Formula
   desc "Binary OS X port of Ken Silverman's PNGOUT by Jonathon Fowler"
   homepage "https://www.jonof.id.au/kenutils.html"
-  url "https://www.jonof.id.au/files/kenutils/pngout-20200115-macos.zip"
-  sha256 "3272fa947eeafc1ac0086e1f14935f9efa21d090a555e6892255fe5c88f6686e"
+  url "https://www.jonof.id.au/files/kenutils/pngout-20230322-mac.zip"
+  sha256 "2e3eb79345206040ae3a0d0d0ecfe9ad01d92fe5002b8a1676a65632a56840e1"
 
   def install
     bin.install "pngout"
+  end
+
+  test do
+    system "#{bin}/pngout", "-force", test_fixtures("test.png"), "out.png"
   end
 end
