398316a7bdb9bc43280a7e45379875e08c9ad86f
Timothy Sutton <tim@synthist.net>
2024-06-24 15:31:24 -0400

    speedwagon: style

--- speedwagon.rb
+++ speedwagon.rb
@@ -1,14 +1,11 @@
 class Speedwagon < Formula
   desc "Download iOS/tvOS/watchOS simulators without Xcode"
   homepage "https://github.com/timsutton/speedwagon"
+  url "https://github.com/timsutton/speedwagon/releases/download/v0.2.0/speedwagon_0.2.0_darwin_all.zip"
+  sha256 "60b2450a4067d6d33fa1821dd26f98a2ab63612d379a23667f1387dbc472c026"
   license "MIT"
 
   head "https://github.com/timsutton/speedwagon.git", branch: "main"
-
-  stable do
-    url "https://github.com/timsutton/speedwagon/releases/download/v0.2.0/speedwagon_0.2.0_darwin_all.zip"
-    sha256 "60b2450a4067d6d33fa1821dd26f98a2ab63612d379a23667f1387dbc472c026"
-  end
 
   depends_on "go" => :build
 
